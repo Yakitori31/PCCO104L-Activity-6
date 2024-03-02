8d892efdbd78b65a3743d0334140da4c21a13b4d
Yakitori31 <158657248+Yakitori31@users.noreply.github.com>
2024-03-02 03:31:53 +0000

    TRY

--- main.cs
+++ main.cs
@@ -2,6 +2,52 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine ("Choose a program (1-5) or type exit to close the program");
+    string userInput = Console.ReadLine();
+
+    if (userInput.ToLower() == "exit")
+    {
+      Console.WriteLine("Closing program.");
+      break;
+    }
+    
+    if (int.TryParse(userInput, out int programNumber)) && programNumber >= 1 && programNumber <= 5)
+    {
+      RunProgram(programNumber);
+    }
+      
+    else
+    {
+      Console.WriteLine("Invalid input. Please enter a number between 1 and 5 or type exit to close the program.");
+    }  
   }
+}
+
+static void RunProgram(int programNumber)
+{
+  switch (programNumber)
+  {
+  case 1:
+    Program1();
+    break;
+  case 2:
+    Program2();
+    break;
+  case 3:
+    Program3();
+    break;
+  case 4:
+    Program4();
+    break;
+  case 5:
+    Program5();
+    break;
+  }
+}
+
+static void Program1()
+{
+Console.
+}
+
 }
